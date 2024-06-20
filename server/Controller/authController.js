640bd956c08ab498dcc40a0c986aad474f7236b6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 07:08:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -38,6 +38,9 @@ const authController = {
                 }
             }
         })
+    },
+    SignIn: (req, res) => {
+        
     }
 }
 
--- server/Routes/authRoute.js
+++ server/Routes/authRoute.js
@@ -4,5 +4,6 @@ const authController = require('../Controller/authController')
 const router = express.Router()
 
 router.post('/SignUp', authController.SignUp)
+router.post('/SignIn')
 
 module.exports = router
