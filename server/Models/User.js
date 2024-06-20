971533ede11dc63f6822926acf94992ec8236789
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 06:35:53 +0530

    Update User.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Models/User.js
+++ server/Models/User.js
@@ -1,5 +1,9 @@
 const connection = require('../config/connection')
 
 const User = {
-    
+    SignUp: (userData, callback) => {
+        db.query('INSERT INTO users SET ?', userData, callback);
+    },
 }
+
+module.exports = User
