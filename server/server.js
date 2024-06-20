49b354fc31b9b89a14cac96079c5db4189261019
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 06:28:00 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const JkMysql = require('jkmysql-easy');
 
+const PORT = process.env.PORT || 8081
+
 // make connection between server and mysql database
 const connection = JkMysql.ConnectToDatabase('localhost', 'root', '1234', 'mysqlmvc')
 
@@ -12,7 +14,5 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
+app.listen(PORT, () => console.log(`Server is Running on PORT ${PORT}`));
