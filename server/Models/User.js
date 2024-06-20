6ca4993a77a605a66df2fe489d82d6eb7a4640a0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 06:52:12 +0530

    Update User.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Models/User.js
+++ server/Models/User.js
@@ -4,6 +4,10 @@ const User = {
     create: (userData, callback) => {
         db.query('INSERT INTO users SET ?', userData, callback);
     },
+    findByUsernameOrEmail: (username, email, callback) => {
+        const query = 'SELECT * FROM users WHERE username = ? OR email = ?';
+        db.execute(query, [username, email], callback);
+    }
 }
 
 module.exports = User
