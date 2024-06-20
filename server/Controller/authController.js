2310ab921497b819b63459228082b68356cf92f7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 06:48:25 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/Controller/authController.js
+++ server/Controller/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../Models/User')
 
 const authController = {
     SignUp: async (req, res) => {
@@ -13,7 +14,15 @@ const authController = {
         const hashPass = await bcrypt.hash(password, 10)
 
         if(hashPass){
-            
+            User.create({
+                username: username,
+                email: email, 
+                password: hashPass, 
+                role: "User", 
+                create_at: new Date(),
+                is_active: 1,
+                is_lock: 0
+            })
         }
         else{
             return res.json({ Error: "Error White Hashing Password" })
