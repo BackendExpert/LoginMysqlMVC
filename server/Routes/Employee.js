a11dd20c6ac0be56084a8dab81d4e355e051ee78
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-27 08:04:00 +0530

    Create Employee.js

---
+++ server/Routes/Employee.js
@@ -0,0 +1,9 @@
+const express = require('express')
+// import your Controller according to route
+    
+const router = express.Router()
+    
+// adding routes according to your needs 
+    
+    
+module.exports = router;
